5d8a309ad07e379793e67486cf2d9099ba43fe9e
aydin <huaydin@student.42vienna.com>
2023-01-07 15:33:15 +0100

    a

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: huaydin <huaydin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 10:51:47 by huaydin           #+#    #+#             */
-/*   Updated: 2022/11/02 22:47:27 by huaydin          ###   ########.fr       */
+/*   Updated: 2023/01/07 15:33:00 by huaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	ft_format(char c, va_list *ap, int *ptr_len)
 
 int	ft_printf(const char *format, ...)
 {
-	int	len;
+	int		len;
 	va_list	ap;
 
 	len = 0;
@@ -59,7 +59,7 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format != '%')
-			len += write(1, &*format, 1);
+			len += write(1, format, 1);
 		if (*format == '%')
 			ft_format(*++format, &ap, &len);
 		format++;
